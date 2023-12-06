0c298484381d6794d135f1ec35048166d7e55158
xianan <101491184+pengyuebin@users.noreply.github.com>
2023-12-06 21:11:45 +0800

    Update zddl.py

--- zddl.py
+++ zddl.py
@@ -1,24 +1,36 @@
+import time
 import requests
 
 # 在这里填写你的用户名和密码
-username = 'g17881101711'
-password = '123123'
+username = '你的账号'
+password = '密码'
 
-# 登录页面的 URL
-login_url = 'http://36.189.241.20:9956/web'
+# 定义连接、断开连接和再次连接的函数
+def connect_disconnect_reconnect():
+    # 构造登录请求的数据
+    login_data = {
+        'web-auth-user': username,
+        'web-auth-password': password,
+        'web-auth-connect': '连接'
+    }
 
-# 创建一个会话
-session = requests.Session()
+    # 发送登录请求
+    response = requests.post('http://36.189.241.20:9956/web', data=login_data)
 
-# 发送登录请求
-login_data = {
-    'username': username,
-    'password': password
-}
-response = session.post(login_url, data=login_data)
+    # 延时一段时间后进行断开连接
+    time.sleep(3)
 
-# 检查登录是否成功
-if response.status_code == 200:
-    print('登录成功')
-else:
-    print('登录失败')
+    # 构造断开连接请求的数据
+    disconnect_data = {
+        'web-auth-disconnect': '断开'
+    }
+
+    # 发送断开连接请求
+    response = requests.post('http://36.189.241.20:9956/web', data=disconnect_data)
+
+    # 延时一段时间后再次连接
+    time.sleep(3)
+    response = requests.post('http://36.189.241.20:9956/web', data=login_data)
+
+# 执行连接、断开连接和再次连接的操作
+connect_disconnect_reconnect()
