0a06441d242f95207aa40b9f7612acd0b4d3f0e5
xianan <101491184+pengyuebin@users.noreply.github.com>
2023-12-06 17:26:25 +0800

    Create zddl.py

---
+++ zddl.py
@@ -0,0 +1,24 @@
+import requests
+
+# 在这里填写你的用户名和密码
+username = '你的账号'
+password = '密码'
+
+# 登录页面的 URL
+login_url = 'http://36.189.241.20:9956/web'
+
+# 创建一个会话
+session = requests.Session()
+
+# 发送登录请求
+login_data = {
+    'username': username,
+    'password': password
+}
+response = session.post(login_url, data=login_data)
+
+# 检查登录是否成功
+if response.status_code == 200:
+    print('登录成功')
+else:
+    print('登录失败')
