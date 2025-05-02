ec6eaa7c4a000ca5849d1683245de5f1a9e02b12
xianan <101491184+xiananrain@users.noreply.github.com>
2025-05-02 15:34:22 +0800

    Update zdll.sh

--- zdll.sh
+++ zdll.sh
@@ -7,23 +7,14 @@ while true; do
     ping -c 3 baidu.com > /dev/null 2>&1
     ping_status=$?
 
-        # 假设 WAN 口为 eth0
-        wan_interface="eth0"
-        macdizhi="mac地址"
-        zhanghao="账号"
-        mima="123123"
+    # 假设 WAN 口为 eth0.2
+    wan_interface="eth0.2"
 
     if [ $ping_status -ne 0 ]; then
-        echo "无法 ping 通 baidu.com，正在重启 wan 接口..."
-        # 重启 wan 接口
-        ifdown wan
-        ifup wan
 
-        # 等待30s
-        echo "等待30s后再次尝试..."
-        sleep 30
-
-
+        # 等待10s
+        echo "等待10s后发送post"
+        sleep 10
 
         # 获取 WAN 口的 IP 地址
         userip=$(ip -4 addr show $wan_interface | grep -oE 'inet ([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{print $2}' | head -n 1)
@@ -31,10 +22,10 @@ while true; do
         if [ -z "$userip" ]; then
             echo "无法获取 $wan_interface 接口的 IP 地址，请检查网络配置。"
         else
-            redirect_url="http://36.189.241.20:9956/?userip=$userip&wlanacname=&nasip=117.191.7.53&usermac=$macdizhi"
+            redirect_url="http://36.189.241.20:9956/?userip=$userip&wlanacname=&nasip=117.191.7.53&usermac=mac地址"
             encoded_redirect_url=$(echo "$redirect_url" | sed 's/&/%26/g' | sed 's/:/%3A/g' | sed 's/\//%2F/g')
 
-            curl 'http://36.189.241.20:9956/web/connect' \
+            response=$(curl -s -o /dev/null -w "%{http_code}" 'http://36.189.241.20:9956/web/connect' \
               -H 'Accept: */*' \
               -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8' \
               -H 'Connection: keep-alive' \
@@ -44,8 +35,14 @@ while true; do
               -H 'Referer: http://36.189.241.20:9956/web' \
               -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36' \
               -H 'X-Requested-With: XMLHttpRequest' \
-              --data-raw "web-auth-user=$zhanghao&web-auth-password=$mima&remember-credentials=false&redirect-url=$encoded_redirect_url" \
-              --insecure
+              --data-raw "web-auth-user=账号&web-auth-password=123123&remember-credentials=false&redirect-url=$encoded_redirect_url" \
+              --insecure)
+
+            if [ $response -eq 200 ]; then
+                echo "POST 请求成功，状态码: $response"
+            else
+                echo "POST 请求失败，状态码: $response"
+            fi
         fi
     else
         echo "网络正常，无需操作。"
