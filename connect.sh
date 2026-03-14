398577dabc5c87dbd544dbc74106e3134133740f
xianan <101491184+xiananrain@users.noreply.github.com>
2026-03-14 19:30:37 +0800

    改为自动获取mac地址

--- connect.sh
+++ connect.sh
@@ -4,7 +4,7 @@
 TARGET_URLS="https://www.baidu.com https://www.qq.com https://www.douyin.com"  # 三个检测地址
 URL_COUNT=3  # URL 数量，和上面的地址数保持一致
 wan_interface="wan"               # WAN接口名称
-zhanghao="gxx"            # 认证账号
+zhanghao="gxx"                    # 认证账号
 mima="123123"                     # 认证密码（一般是123123）
 MIN_INTERVAL=240                  # 最小检测间隔（秒），4分钟
 MAX_INTERVAL=600                  # 最大检测间隔（秒），10分钟
